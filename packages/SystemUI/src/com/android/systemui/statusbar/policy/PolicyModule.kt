cbdfe8d347d341806124aeeebfe9cf8ca037e9cd
superuseryu <superuseryu@proton.me>
2026-08-18 23:14:04 +0000

    SystemUI: Add Sound QS tile with 3-state ringer mode indicator
    Change-Id: I9122d19c0421374d3823f20af8b766aaf4efc3df

--- packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
+++ packages/SystemUI/src/com/android/systemui/statusbar/policy/PolicyModule.kt
@@ -78,6 +78,7 @@ import com.android.systemui.qs.tiles.impl.work.domain.interactor.WorkModeTileDat
 import com.android.systemui.qs.tiles.impl.work.domain.interactor.WorkModeTileUserActionInteractor
 import com.android.systemui.qs.tiles.impl.work.domain.model.WorkModeTileModel
 import com.android.systemui.qs.tiles.impl.work.ui.mapper.WorkModeTileMapper
+import com.android.systemui.qs.tiles.SoundTile
 import com.android.systemui.res.R
 import dagger.Binds
 import dagger.Module
@@ -518,4 +519,10 @@ interface PolicyModule {
     @IntoMap
     @StringKey(UiModeNightTile.TILE_SPEC)
     fun bindUiModeNightTile(uiModeNightTile: UiModeNightTile): QSTileImpl<*>
+
+    /** Inject SoundTile into tileMap in QSModule */
+    @Binds
+    @IntoMap
+    @StringKey(SoundTile.TILE_SPEC)
+    fun bindSoundTile(soundTile: SoundTile): QSTileImpl<*>
 }
